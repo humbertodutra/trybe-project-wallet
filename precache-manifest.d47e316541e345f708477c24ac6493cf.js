54deb9d9956f54244c38809fb1a4dafea2c3f9bc
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-02-09 17:39:52 -0300

    Updates

---
+++ precache-manifest.d47e316541e345f708477c24ac6493cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa54a54fc2dc66494700f7665b59239",
+    "url": "/trybe-project-wallet/index.html"
+  },
+  {
+    "revision": "49d522c00a8d1781bd24",
+    "url": "/trybe-project-wallet/static/css/main.34e106e5.chunk.css"
+  },
+  {
+    "revision": "40454000e0c3117fa682",
+    "url": "/trybe-project-wallet/static/js/2.ca754cd7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-project-wallet/static/js/2.ca754cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d522c00a8d1781bd24",
+    "url": "/trybe-project-wallet/static/js/main.35e1f982.chunk.js"
+  },
+  {
+    "revision": "8899a4205afc779eb859",
+    "url": "/trybe-project-wallet/static/js/runtime-main.75738b09.js"
+  },
+  {
+    "revision": "6408b50085d19252bf348ec07e68668e",
+    "url": "/trybe-project-wallet/static/media/wallet.6408b500.png"
+  }
+]);
